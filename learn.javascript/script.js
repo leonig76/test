75ab11f2aa7a20880beff383527a225e98bfbcb2
Igor <lobosu12@gmail.com>
2020-03-03 12:13:52 +0300

    Это наш первый комит

---
+++ learn.javascript/script.js
@@ -0,0 +1,59 @@
+// Однострочный комментарий
+/*
+Многострочный
+комментарий
+*/
+
+'use strict';
+
+
+
+//Object
+
+// let persone = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+
+// };
+
+//console.log(persone.name);
+//console.log(persone['name']);
+
+
+//Array
+
+// let arr = ['orange.png', 'plum.jpg', 'apple.bmp'];
+
+// console.log(arr[2]); //apple.bmp
+
+
+//Простое общение с пользователем
+
+
+//alert('Hello, world!');
+
+// let answer = confirm('Will you join us?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18 лет?', '2423');
+
+// console.log(typeof (answer) ); // string
+// console.log(typeof (arr) ); // string
+
+// Операторы в JS
+
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+
+
+
+
+
